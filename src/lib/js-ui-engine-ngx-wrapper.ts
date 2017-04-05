72a1fd1888c57f0de7ec635fe4eb20dc0b6455e5
Steven Lawler <steven.lawler777@gmail.com>
2017-04-04 23:18:29 -0400

    Got module being loaded

--- src/lib/js-ui-engine-ngx-wrapper.ts
+++ src/lib/js-ui-engine-ngx-wrapper.ts
@@ -1,6 +1,14 @@
 
 export class JSUIEngine {
+
+    private jsuiEngine:any = require("../../node_modules/js-ui-engine/src/canvas_ui.js");
+
     constructor() {
         console.log("Works!");
     }
+
+    public static test() {
+        console.log("yay");
+    }
+
 }
